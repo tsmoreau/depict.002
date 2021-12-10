7aedf08e31eabb67538ab2da7a1c6b4481b66273
tsmoreau <64439028+tsmoreau@users.noreply.github.com>
2021-12-10 11:20:13 -0800

    Initial commit

---
+++ config.js
@@ -0,0 +1,5 @@
+export const sharkTokenAddress = "0x2A0C59102cD582c2D368C2ce5c31B443A3F4Bf72";
+export const sharkCalculatorAddress = "";
+export const sharkAssemblerAddress = "";
+export const sharkStorageAddress = "0xa3BfCCCEDD2a37aDAe95272A260Ec897B81De653";
+export const IPFSgateway = "https://ipfs.infura.io/ipfs/";
